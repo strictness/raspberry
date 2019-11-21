b23c024d241f563ab4553b06ad56f2d313332a1d
CR <robert.bilkevic@gmail.com>
2019-11-22 00:20:00 +0200

    Removed overhead

--- main.py
+++
@@ -1,8 +0,0 @@
-from raspberry import config
-from raspberry import app
-
-if __name__ == "__main__":
-    app.app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
-    app.app.config['MAX_CONTENT_LENGTH'] = config.MAX_CONTENT_LENGTH
-
-    app.app.run(host='0.0.0.0', port=config.PORT, debug=config.DEBUG_MODE)
